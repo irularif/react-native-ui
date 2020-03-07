d8c335266edc6441a2ad4d9ed77bf02493a07f56
IrulArif <masterirul@gmail.com>
2020-03-07 21:00:53 +0700

    fix

---
+++ UI/Modal/index.tsx
@@ -0,0 +1,36 @@
+import Constants from "expo-constants";
+import { observer } from "mobx-react-lite";
+import React from "react";
+import {
+  Modal,
+  ModalProps as ModalPropsOrigin,
+  Platform,
+  StyleSheet
+} from "react-native";
+import Screen from "../Screen";
+
+export interface ModalProps extends ModalPropsOrigin {
+  style?: any;
+  children?: any;
+}
+
+export default observer((props: ModalProps) => {
+  const { style, children } = props;
+  const marginTop = Platform.OS === "android" ? -Constants.statusBarHeight : 0;
+  const baseStyle = {
+    backgroundColor: "transparent"
+  };
+  const cstyle = StyleSheet.flatten([baseStyle, style]);
+  return (
+    <Modal animationType="fade" transparent={true} {...props}>
+      <Screen
+        statusbarStyle={{
+          marginTop
+        }}
+        style={cstyle}
+      >
+        {children}
+      </Screen>
+    </Modal>
+  );
+});
